65d65b0691a6c517741caf47d026838fbcf64b60
Istiak <istieak.hasan1996@gmail.com>
2022-05-12 17:34:49 -0700

    info component added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from './pages/Shared/Navbar';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto px-12'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}></Route>
--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png';
-// import PrimaryButton from '../Shared/PrimaryButton';
+import PrimaryButton from '../Shared/PrimaryButton';
 
 const Banner = () => {
     return (
@@ -10,9 +10,9 @@ const Banner = () => {
                 {/* <img src={chair} className="max-w-sm rounded-lg shadow-2xl" /> */}
                 <div>
                     <div className='w-10/12 mx-auto'>
-                    <h1 className="text-5xl font-bold w-9/12">Your New Smile Starts Here</h1>
+                    <h1 className="text-5xl font-bold ">Your New Smile Starts Here</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    {/* <PrimaryButton>Get Started</PrimaryButton> */}
+                     <PrimaryButton>Get Started</PrimaryButton> 
                     </div>
                 </div>
             </div>
